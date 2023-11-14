32cd7e138dc3b473a3ccb62009e2872b5ba28ab2
Hanspeter <hansdietiker2012@gmail.com>
2023-11-14 15:56:00 -0300

    Poo_CSharp

--- PooCSharp/Entities/Order.cs
+++ PooCSharp/Entities/Order.cs
@@ -1,17 +1,60 @@
 using System;
+using System.Globalization;
+using System.Text;
 using PooCSharp.Entities.enums;
 
 namespace PooCSharp.Entities;
 
 public class Order
 {
-    public int Id { get; set; }
     public DateTime Moment { get; set; }
     public OrderStatus Status { get; set; }
+    public Client Client { get; set; }
+    public List<OrderItem> Item { get; set; } = new List<OrderItem>();
 
+    public Order()
+    {
+        
+    }
+    public Order(Client client, DateTime moment, OrderStatus status)
+    {
+        Client = client;
+        Moment = moment;
+        Status = status;
+    }
 
+    public void AddItem(OrderItem addItem)
+    {
+        Item.Add(addItem);
+    }
+
+    public void RemoveItem(OrderItem removeItem)
+    {
+        Item.Remove(removeItem);
+    }
+
+    public double Total()
+    {
+        double sum = 0.0;
+        foreach (OrderItem item in Item)
+        {
+            sum += item.SubTotal();
+        }
+
+        return sum;
+    }
     public override string ToString()
     {
-        return Id + ", " + Moment + ", " + Status;
+        StringBuilder sb = new StringBuilder();
+        sb.AppendLine("Order moment: " + Moment.ToString("dd/MM/yyyy HH:mm:ss"));
+        sb.AppendLine("Order status: " + Status);
+        sb.AppendLine("Client: " + Client);
+        sb.AppendLine("Order items:");
+        foreach (OrderItem item in Item)
+        {
+            sb.AppendLine(item.ToString());
+        }
+        sb.AppendLine("Total price: $" + Total().ToString("F2", CultureInfo.InvariantCulture));
+        return sb.ToString();
     }
 }
--- PooCSharp/Program.cs
+++ PooCSharp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using PooCSharp.Entities;
 using PooCSharp.Entities.enums;
 
@@ -9,12 +10,46 @@ namespace PooCSharp
     {
         static void Main(string[] args)
         {
-            Order order = new Order
+            Console.WriteLine("Enter Client Data:");
+            Console.Write("Name:");
+            string clientName = Console.ReadLine();
+
+            Console.Write("Email:");
+            string clientEmail = Console.ReadLine();
+
+            Console.Write("Birth Date (DD/MM/YYYY)");
+            DateTime clientBirth = DateTime.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter order Data:");
+            OrderStatus orderStatus = Enum.Parse<OrderStatus>(Console.ReadLine());
+
+            Client client = new Client(clientName, clientEmail, clientBirth);
+            Order order = new Order(client, DateTime.Now, orderStatus);
+
+            Console.Write("How many items to this order:");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i < n; i++)
             {
-                Id = 1080,
-                Moment = DateTime.Now,
-                Status = OrderStatus.PendingPayment
-            };
+                Console.WriteLine($"Enter #{i} item data:");
+                Console.Write("Product name:");
+                string productName = Console.ReadLine();
+
+                Console.Write("Product price");
+                double productPrice = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Product product = new Product(productName, productPrice);
+
+                Console.Write("Quantity:");
+                int quantity = int.Parse(Console.ReadLine());
+
+                OrderItem orderItem = new OrderItem(quantity, productPrice, product);
+
+                order.AddItem(orderItem);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Order Summary:");
             Console.WriteLine(order);
         }
     }
