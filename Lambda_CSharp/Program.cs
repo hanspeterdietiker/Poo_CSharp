a00ea79ad2c750792b32a743f5c0c70b193722c0
Hanspeter <hansdietiker2012@gmail.com>
2023-11-15 21:39:41 -0300

    Lambda

---
+++ Lambda_CSharp/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Lambda_CSharp;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        //Especifica a fonte de dados
+        int[] numbers = new int[] { 2, 3, 4, 5 };
+
+        // Define a quary(consulta) operaçoes do LINQ
+        var result = numbers.
+            Where(x => x % 2 == 0).
+            Select(x => x * 10);
+        
+        //Executando a quary
+        foreach (int x in result)
+        {
+            Console.WriteLine(x);
+        }
+    }
+}
---
+++ Lambda_CSharp2/entities/Category.cs
@@ -0,0 +1,10 @@
+namespace Lambda_CSharp2.entities;
+
+public class Category
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Tier { get; set; }
+
+   
+}
